1fb3d574fc8db2e372726342a361ac5b991b6764
Keesha Kallasur <keeshakallasur@gmail.com>
2024-06-21 15:10:36 +0200

    Added JS

---
+++ src/script.js
@@ -0,0 +1,56 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#weather-app-temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#weather-app-city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windSpeedElement = document.querySelector("#wind-speed");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+    let iconElement = document.querySelector("#icon");
+  
+    iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"/>`;
+    timeElement.innerHTML = formatDate(date);
+    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+    descriptionElement.innerHTML = response.data.condition.description;
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+  }
+  
+  function formatDate(date) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    let day = days[date.getDay()];
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    return `${day} ${hours}:${minutes}`;
+  }
+  
+  function searchCity(city) {
+    let apiKey = "a77d4b0f7e2617b44oe3t97209d831aa";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+  }
+  
+  function handleSearchSubmit(event) {
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+  
+    searchCity(searchInput.value);
+  }
+  let searchFormElement = document.querySelector("#search-form");
+  searchFormElement.addEventListener("submit", handleSearchSubmit);
+  
+  searchCity("Venice");
+  
